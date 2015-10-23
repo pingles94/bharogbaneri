e79386772422ec2c20edf63e121fde415ba8ac44
Rishabh Parikh <rishabh@berkeley.edu>
2015-10-23 02:06:28 -0700

    remove useless <p> tags

--- js/index.js
+++ js/index.js
@@ -42,29 +42,29 @@ $(document).ready(function() {
 				iconSize: [46,48]
 			});
 
-			//creating markers
+			//creating markers and setting properties
 			var marker1 = L.marker([30.558123, 77.504083], {title: "WARD 1", icon: myIcon})
-										.bindPopup("<p>Ward 1 Info</p>", L.popup({'closeOnClick': true}))
+										.bindPopup("<b>Ward 1 Info</b>", L.popup({'closeOnClick': true}))
 										.on('click', function() { map.setView(marker1.getLatLng(), 15, {animate:true});})
 										.addTo(map);
 
 			var marker2 = L.marker([30.549871, 77.503179], {title: "WARD 2", icon: myIcon})
-										.bindPopup("<p>Ward 2 Info</p>", L.popup({'closeOnClick': true}))
+										.bindPopup("<b>Ward 2 Info</b>", L.popup({'closeOnClick': true}))
 										.on('click', function() { map.setView(marker2.getLatLng(), 15, {animate:true});})
 										.addTo(map);
 
 			var marker3 = L.marker([30.549754, 77.519363], {title: "WARD 3", icon: myIcon})
-										.bindPopup("<p>Ward 3 Info</p>", L.popup({'closeOnClick': true}))
+										.bindPopup("<b>Ward 3 Info</b>", L.popup({'closeOnClick': true}))
 										.on('click', function() { map.setView(marker3.getLatLng(), 15, {animate:true});})
 										.addTo(map);
 
 			var marker4 = L.marker([30.542396, 77.508649], {title: "WARD 4", icon: myIcon})
-										.bindPopup("<p>Ward 4 Info</p>", L.popup({'closeOnClick': true}))
+										.bindPopup("<b>Ward 4 Info</b>", L.popup({'closeOnClick': true}))
 										.on('click', function() { map.setView(marker4.getLatLng(), 15, {animate:true});})
 										.addTo(map);
 
 			var marker5 = L.marker([30.562289, 77.516831], {title: "WARD 5", icon: myIcon})
-										.bindPopup("<p>Ward 5 Info</p>", L.popup({'closeOnClick': true}))
+										.bindPopup("<b>Ward 5 Info</b>", L.popup({'closeOnClick': true}))
 										.on('click', function() { map.setView(marker5.getLatLng(), 15, {animate:true});})
 										.addTo(map);
 
