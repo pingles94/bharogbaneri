11aaba6df27cb719f036898a36580d6d969f483d
Rishabh Parikh <rishabh@berkeley.edu>
2015-11-03 17:56:34 -0800

    Sample 2 Initial commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
-
-	$('#first').css('opacity', '0.0');;
-	$('#second').css('opacity', '0.0');;
-	$('.smallQuote').css('opacity', '0.0');;
-
-	$('#first').delay(500).fadeTo(2000, 1);
-	$('#second').delay(2000).fadeTo(2000, 1);
-	$('.smallQuote').delay(3500).fadeTo(2000, 0.5);
+	$('#first').fadeOut(0);
+	$('#first').delay(500).fadeIn(1000);
 });
